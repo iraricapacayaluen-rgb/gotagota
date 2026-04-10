a19894fae17462583bde87a35981ef94fb8c81a5
iraricapacayaluen-rgb <iraricapacayaluen@gmail.com>
2026-04-10 14:19:17 -0500

    a

---
+++ frontend/js/main.js
@@ -0,0 +1,12 @@
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('http://localhost:8080/api/clientes')
+        .then(response => response.json())
+        .then(data => {
+            
+            //console.log('Datos:', data);
+            const elemento = document.getElementById("table-cliente")
+            elemento.innerHTML = JSON.stringify(data);
+            console.log(elemento)
+        })
+});
